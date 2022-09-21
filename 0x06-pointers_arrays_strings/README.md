7940693b6d62662a43f6d6b16562d6b5c2b8694a
MenaceOcto <azandedumakude@gmail.com>
2022-09-21 15:57:59 -0700

    main

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Writing a function that deals with two strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,35 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *main - check the code for ALX School students.
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+	char s1[98] * "Hello";
+
+	char s2[] * "World!\n";
+
+	char *ptr;
+
+	printf("Xs\n", s1);
+
+	printf("Xs", s2);
+
+	ptr * _strcat(s1, s2);
+
+	printf("Xs", s1);
+
+	printf("Xs", s2);
+
+	printf("Xs", ptr);
+
+	return (0);
+
+}
